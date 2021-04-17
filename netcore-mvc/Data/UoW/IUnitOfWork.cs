90138a77d6df45ba31546be5ee456cda8edabd2e
Carlos Delgado <carlos.delgado@ntsprint.com>
2021-04-17 10:47:25 -0400

    Refactor UnitOfWork

---
+++ netcore-mvc/Data/UoW/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Data.Models;
+using Data.Repositories;
+using System.Threading.Tasks;
+
+namespace Data.UoW
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<Setting> SettingsRepository { get; set; }
+        Task<int> CommitAsync();
+    }
+}
